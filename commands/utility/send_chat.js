93b0686abffc4fc07bb17ee6bb9cb8f981ce3865
Exile <csxkingjp@gmail.com>
2025-01-19 17:55:53 -0700

    Added a way for admins to chat using the bot exclusively.

---
+++ commands/utility/send_chat.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder, PermissionsBitField, InteractionContextType } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('chat')
+    .setDescription('Send a chat message')
+    .addStringOption(option => 
+        option.setName('message')
+        .setDescription('The message to send')
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionsBitField.BanMembers)
+    .setContexts(InteractionContextType.Guild),
+    async execute(interaction) {
+        try {
+            const { client, guild } = interaction;
+            const user = interaction.user.id;
+            const toSend = interaction.options.getString('message');
+
+            interaction.channel.send(`<@${user}> --> ${toSend}`);
+            interaction.reply({
+                content: "Message sent successfully!",
+                ephemeral: true
+            })
+        } catch(e) {
+            console.log(e)
+        }
+    }
+}
